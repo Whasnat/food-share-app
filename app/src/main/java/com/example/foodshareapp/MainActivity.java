b734ca1b7e84a0a266a0869314ea6813e39ac2d2
whasnat <w.hasnat94@gmail.com>
2021-07-12 01:23:40 +0600

    Demo Commit for Assignment

--- app/src/main/java/com/example/foodshareapp/MainActivity.java
+++ app/src/main/java/com/example/foodshareapp/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.foodshareapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +20,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        Log.d("Account type: ", String.valueOf(account));
+
+
+        final SignInButton googleSignIn = (SignInButton) findViewById(R.id.sign_in_button);
+        TextView textView = (TextView) googleSignIn.getChildAt(0);
+        textView.setText("Sign In with Google");
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Google Sign IN", "onClick: google Sign In");
+                Log.d("Account type: ", String.valueOf(account));
+            }
+        });
+
+    }
+
+
+    protected void updateUI(GoogleSignInAccount account){
+        if (account != null){
+            // MainActivity
+        }else{
+            //refresh to show signin btns
+        }
+        return;
+    };
+
+    protected void handleClick(View view){
+
     }
 }
+
